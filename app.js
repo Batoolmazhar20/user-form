7fb70166416b3ea69569307077e4ea3c8daa64c5
Muskaankamran <muskaankamran494@gmail.com>
2026-02-09 15:22:31 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+function previewDate() {
+var email = document.getElementById("email").value;
+var name = document.getElementById("name").value;
+var fname = document.getElementById("fname").value;
+var cnic = document.getElementById("cnic").value;
+var age = document.getElementById("age").value;
+
+var fullName = name + " " + fname;
+
+}
